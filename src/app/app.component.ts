e6114f202094a1606476fd01e1429773913d4376
Michaela Bačíková <michaela.bacikova@tuke.sk>
2019-02-02 16:03:18 +0100

    css styly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-          <!--h1>Toto je moja apka s nazvom {{title}}</h1>
-          <app-auto></app-auto>
-          <app-osoba></app-osoba>
-          <app-test></app-test>
-          <app-osoba-edit></app-osoba-edit> -->
-          <app-osoba-list></app-osoba-list>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
